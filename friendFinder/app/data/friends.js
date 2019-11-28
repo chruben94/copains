69041ff45a62e344df67e65e48c96220609fa9cf
Ruben Chavez <chruben94@gmail.com>
2019-11-27 20:08:21 -0800

    need to add photos to friends js file

---
+++ friendFinder/app/data/friends.js
@@ -0,0 +1,65 @@
+var friends = [
+    {
+      name: "Jorge Mancero",
+      photo:
+        "",
+      scores: [1, 2, 3, 4, 5, 1, 2, 5, 4, 4]
+    },
+    {
+      name: "Claudia Gutarra ",
+      photo:
+        "",
+      scores: [1, 2, 5, 1, 2, 2, 4, 1, 3, 2]
+    },
+    {
+      name: "Jennifer Aguirre",
+      photo:
+        "",
+      scores: [1, 4, 2, 2, 5, 1, 3, 2, 5, 5]
+    },
+    {
+      name: "Lazaro Perez",
+      photo:
+        "",
+      scores: [3, 2, 3, 2, 3, 1, 3, 4, 2, 3]
+    },
+    {
+      name: "Leigh Tracey",
+      photo:
+        "",
+      scores: [3, 3, 3, 2, 4, 3, 5, 3, 5, 4]
+    },
+    {
+      name: "Kevin Ferguson",
+      photo:
+        "",
+      scores: [2, 4, 3, 3, 2, 2, 3, 2, 4, 5]
+    },
+      {
+        name: "Zee Ziad",
+        photo:
+          "",
+        scores: [1, 1, 4, 4, 5, 1, 1, 5, 1, 4]
+      },
+      {
+        name: "Amiel Nicdao",
+        photo:
+          "",
+        scores: [2, 3, 4, 1, 3, 2, 5, 3, 2, 4]
+      },
+      {
+        name: "Santiago Angarita",
+        photo:
+          "",
+        scores: [1, 2, 4, 1, 4, 3, 5, 3, 5, 4]
+      },
+      {
+        name: "Felix Fernandez",
+        photo:
+          "",
+        scores: [2, 4, 3, 3, 4, 2, 3, 1, 4, 5]
+      }
+    
+  ];
+  module.exports = friends;
+  
--- friendFinder/server.js
+++ friendFinder/server.js
@@ -5,6 +5,18 @@ var path = require("path");
 var app = express();
 var port = process.env.PORT || 8282;
 
+//middleware that will pass post data for us and store it
+//returns json and string text
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+//api route
+require("./app/routing/api-routes")(app);
+//home and survey html routes 
+require("./app/routing/html-routes")(app);
+
 app.listen(port, () => console.log("Listening on port", port));
 
 
